2fe92326b7bb875e663c55a002679cd952628bff
szgekko <44727340@qq.com>
2017-03-31 16:18:39 +0800

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,11 +1,34 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller {
 	public function create() {
 		return view('users.create');
+	}
+
+	public function show($id) {
+		$user = User::findOrFail($id);
+		return view('users.show', compact('user'));
+	}
+
+	public function store(Request $request) {
+		$this->validate($request, [
+			'name' => 'required|min:2|max:50',
+			'email' => 'required|email|unique:users|max:255',
+			'password' => 'required|confirmed|min:6',
+		]);
+
+		$user = User::create([
+			'name' => $request->name,
+			'email' => $request->email,
+			'password' => bcrypt($request->password),
+		]);
+
+		session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+		return redirect()->route('users.show', [$user]);
 	}
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -35,4 +35,9 @@ CanResetPasswordContract {
 	 * @var array
 	 */
 	protected $hidden = ['password', 'remember_token'];
+
+	public function gravatar($size = '100') {
+		$hash = md5(strtolower(trim($this->attributes['email'])));
+		return "http://www.gravatar.com/avatar/$hash?s=$size";
+	}
 }
